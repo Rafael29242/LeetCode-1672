00415c48f1ef41846b68570165e69adf2345b6a4
Rafi Gomez <77979382+Rafael29242@users.noreply.github.com>
2021-04-07 19:12:34 -0400

    Add files via upload

---
+++ LeetCode1672/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode1672
+{
+    class Program
+    {
+        public int MaximumWealth(int [][] accounts)
+        {
+
+
+
+            int wealthiest = 0;
+
+            for (int cust = 0; cust < accounts.Length; cust++)// cust is customer
+            {
+                int Wealthsum = 0;
+
+                for (int banks = 0; banks < accounts[cust].Length; banks++)
+                {
+                    Wealthsum += accounts[cust][banks];
+                }
+
+                wealthiest = Math.Max(wealthiest, Wealthsum);
+
+                //if(Wealthsum > wealthiest)
+                //{
+                //    wealthiest = Wealthsum;
+                //}
+                    
+            }
+                return wealthiest;
+
+
+        }
+    }
+}
